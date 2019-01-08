b4452b1652658cd769cf2dfb9130f6d6ac6925ee
cristij <joracristi@gmail.com>
2019-01-08 17:57:45 +0200

    Updates

---
+++ precache-manifest.e7409bf4ada5b5ee937ba0f5e09222b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-component-editor/robots.txt"
+  },
+  {
+    "revision": "ad7f709295492e38d3b7",
+    "url": "/vue-component-editor/js/chunk-vendors.4278d390.js"
+  },
+  {
+    "revision": "408b6f72fd5e30c64b35",
+    "url": "/vue-component-editor/js/app.dce86952.js"
+  },
+  {
+    "revision": "13171516c36ae7fb31980eaf3178eb8c",
+    "url": "/vue-component-editor/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-component-editor/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/vue-component-editor/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/vue-component-editor/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "ad7f709295492e38d3b7",
+    "url": "/vue-component-editor/css/chunk-vendors.3272d0d8.css"
+  },
+  {
+    "revision": "408b6f72fd5e30c64b35",
+    "url": "/vue-component-editor/css/app.e24af020.css"
+  }
+];
